f56cbbd09801b87e91c446fb6ce6ba8d5bfd4e29
Elisabetta <Elisabetta@172.21.56.77>
2019-03-18 13:36:13 +0100

    Implementato punto 1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,38 @@
 package it.polito.tdp.libretto;
 
-public class Libretto {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Libretto {
+	
+	// conosciamo sia Linked che Array Sono classi,che implementano l'interfaccia. List è un' interfaccia predefinita dentro java.util
+	private List <Voto> voti ;
+    // LinkedList e ArrayList dal punto di vista funzionale svolgono la stessa funzione, dal punto di vista delle prestazioni no.
+	// IMPORTANTE !! quando possibile, definire le variabili nel modo più generale possibile: List. Pattern da usare sempre. Nel costruttore specificherò la classe.
+	
+	public Libretto() {
+		this.voti= new ArrayList<Voto>();
+	}
+	
+	
+	// due versioni. qual è più utile? nel primo tipo ho i tre pezzi, mi devo creare l'oggetto voto e poi aggiungerlo alla lista. 
+	// nel secondo metodo devo fare il richiamante.Cioè fuori dalla classe Libretto. 
+	// la seconda soluzione è migliore. Perchè? perchè cosi è possibile modificare la classe interna senza dover modificare anche qui  
+	
+	
+	public void add(int voto, String corso, LocalDate data) {
+		
+	}
+	
+	
+	/**
+	 * Aggiungere un voto al libretto
+	 * @param v il voto da aggiungere
+	 */
+     public void add(Voto v) {
+    	 voti.add(v);
+		
+	}
+	
 }
---
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -0,0 +1,30 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+
+public class TestLibretto {
+
+	//perchè una classe separata?mi permette di essere sicuro che io non usi dati privati neanche per sbaglio. 
+	// richio anche di vedere una variabile private ( es il voto ). Non sarebbe un test realistico. 
+	
+	
+	//questa classe non stampa niente,non da errori,ma c'è un altro modo per vedere se il mio p
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Libretto libr= new Libretto();
+		libr.add(new Voto(30,"Analisi I",LocalDate.of(2017, 1, 15)));
+		libr.add(new Voto(21,"Analisi II",LocalDate.of(2018, 1, 15)));
+		libr.add(new Voto(25,"Fisica I",LocalDate.of(2017, 6, 10)));
+		libr.add(new Voto(28,"Fisica II",LocalDate.of(2018, 9, 3)));
+		libr.add(new Voto(18,"Geometria",LocalDate.of(2017, 1, 15)));
+		libr.add(new Voto(20,"Economia",LocalDate.of(2017, 1, 15)));
+		libr.add(new Voto(25,"RicercaOperativa",LocalDate.of(2018, 1, 28)));
+		libr.add(new Voto(24,"Complementi",LocalDate.of(2018, 6, 5)));
+		libr.add(new Voto(25,"Logistica",LocalDate.of(2018, 2, 15)));
+		libr.add(new Voto(27,"Programmazione a oggetti",LocalDate.of(2019, 1, 25)));
+
+	}
+
+}
